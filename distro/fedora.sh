6a0feab3152b2febc8f2b1ab440f180a8b7b5c58
Jan <janbuchwinkler@gmail.com>
2025-07-29 16:00:35 +0200

    removed . vor cp

--- distro/fedora.sh
+++ distro/fedora.sh
@@ -85,17 +85,17 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 echo "=> Installiere Alacritty-Konfiguration..."
   mkdir -p ~/.config/alacritty
-  cp ./config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+  cp /config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
   echo "✓ Alacritty-Konfiguration kopiert."
 
 echo "=> Installiere Fastfetch-Konfiguration..."
   mkdir -p ~/.config/fastfetch
-  cp ./config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+  cp /config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
   echo "✓ Fastfetch-Konfiguration kopiert."
 
 echo "=> zsh config"
- cp ./config/zsh/.zshrc ~/
- cp ./config/zsh/.p10k.zsh ~/
+ cp /config/zsh/.zshrc ~/
+ cp /config/zsh/.p10k.zsh ~/
  echo "fertig"
 
 echo "🧩 Adding Flathub repository..."
